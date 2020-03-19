252b2d139174382740c840ebff7619369f9a4681
sergeystyagailo <startupprojectvl@gmail.com>
2020-03-19 14:33:01 +0300

    RemovedDuplicatesSolution updated

--- algorthms/easy/26.RemoveDuplicatesfromSortedArray.kt
+++ algorthms/easy/26.RemoveDuplicatesfromSortedArray.kt
@@ -18,3 +18,17 @@ fun removeDuplicates(nums: IntArray): Int {
         }
         return result
 }
+
+fun removeDuplicatesImproved(nums: IntArray): Int {
+    if (nums.size == 0) return 0
+
+    var i = 0
+    for (j in 1 until nums.size) {
+        if (nums[i] != nums[j]) {
+            i++
+            nums[i] = nums[j]
+        }
+    }
+
+    return i + 1
+}
